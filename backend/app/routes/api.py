34a29341befeb270bfcdab73721ca288cd4102ed
SilencedSign87 <diazvegabruno@hotmail.com>
2026-07-03 22:52:01 -0500

    Añadido documentación a /health

--- backend/app/routes/api.py
+++ backend/app/routes/api.py
@@ -2,7 +2,12 @@ from flask_openapi3 import APIBlueprint
 
 api_bp = APIBlueprint('api', __name__)
 
-@api_bp.get('/health')
+@api_bp.get(
+        '/health',
+        responses={
+            '200': {'description': 'OK'},
+        }
+        )
 def health():
     """Health check de la API"""
     return {'status': 'ok'}
