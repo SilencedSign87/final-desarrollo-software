7cf0a739daf7e8905bbb390bc74452a783185d8e
SilencedSign87 <diazvegabruno@hotmail.com>
2026-07-13 19:40:16 -0500

    mas cambios de nombre para despliegue

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import Spinner from "./spinner";
+import Spinner from "./Spinner";
 import LoadingView from "../views/LoadingView";
 
 export default function ProtectedRoute({ children, allowedRoles }) {
